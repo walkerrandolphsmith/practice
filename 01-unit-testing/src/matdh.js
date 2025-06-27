edf04ac0247d9a3b33feb0f88f2d773e742f58d3
walkersmith@nuc-14 <walkerrandolphsmith@gmail.com>
2025-06-27 16:16:02 -0400

    solve problem

---
+++ 01-unit-testing/src/matdh.js
@@ -0,0 +1,11 @@
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: {{ .Release.Name }}
+  labels:
+    app: {{ .Release.Name }}
+spec:
+  replicas: {{ .Values.replicaCount }}
+  selector:
+    matchLabels:
+      app: {{ .Release.Name }}
